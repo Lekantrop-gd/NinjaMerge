7e59177ea4fbb37b9a6de5466f82898a30c6a5f6
Nazar Holodiuk <98541354+Lekantrop-gd@users.noreply.github.com>
2024-03-29 14:59:43 -0500

    Update mergable

--- Assets/Scripts/Mergable.cs
+++ Assets/Scripts/Mergable.cs
@@ -1,9 +1,17 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Merge
 {
     public class Mergable : MonoBehaviour
     {
+        [SerializeField] private Image _image;
+
+        public void Init(Sprite image)
+        {
+            _image.sprite = image;
+        }
+
         public void Put(Mergable mergable)
         {
 
---
+++ Assets/Scripts/MergableSpawner.cs
@@ -0,0 +1,40 @@
+﻿using Items;
+using Merge;
+using NaughtyAttributes;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    public class MergableSpawner : MonoBehaviour
+    {
+        [SerializeField] private GameObject _mergingDesk;
+        [SerializeField] private Mergable _mergable;
+        [SerializeField] private Sword[] _swords;
+        [SerializeField] private Armor[] _armor;
+        [SerializeField] private Hat[] _armorHat;
+        [SerializeField] private int _maxItems;
+
+        [Button]
+        public void SpawnRandowSword()
+        {
+            if (_mergingDesk.transform.childCount < _maxItems)
+            {
+                Mergable mergable = Instantiate(_mergable, _mergingDesk.transform);
+                mergable.Init(_swords[0].Sprite);
+            }
+        }
+
+        [Button]
+        public void SpawnRandowArmor()
+        {
+
+        }
+
+        [Button]
+        public void SpawnRandowHat()
+        {
+
+        }
+    }
+}
