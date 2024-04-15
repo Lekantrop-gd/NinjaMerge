93633efd53142d9065202d9b6a45742842f84ab0
Nazar Holodiuk <98541354+Lekantrop-gd@users.noreply.github.com>
2024-04-15 12:03:29 -0500

    Add particles logic

--- Assets/Scripts/CellsGrid.cs
+++ Assets/Scripts/CellsGrid.cs
@@ -1,6 +1,5 @@
 using NaughtyAttributes;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 public class CellsGrid : MonoBehaviour
--- Assets/Scripts/Mergable.cs
+++ Assets/Scripts/Mergable.cs
@@ -1,8 +1,26 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Mergable : MonoBehaviour
 {
     [SerializeField] private Mergable _superior;
+    [SerializeField] private ParticleSystem _appearParticles;
 
     public Mergable Superior => _superior;
+
+    private void OnEnable()
+    {
+        GameObject particles = Instantiate(_appearParticles).gameObject;
+        particles.transform.position = transform.position;
+
+        particles.GetComponent<ParticleSystem>().Play();
+
+        float totalDuration = particles.GetComponent<ParticleSystem>().main.duration + particles.GetComponent<ParticleSystem>().startLifetime;
+        Destroy(particles, totalDuration);
+    }
+
+    private void OnDisable()
+    {
+
+    }
 }
